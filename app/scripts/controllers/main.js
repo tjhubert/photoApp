447dce3d635c18c0b5be105f3f74222c56a26228
tjhubert <tjhubert@live.com>
2015-01-09 20:49:52 +0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name photoAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the photoAppApp
+ */
+angular.module('photoAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
